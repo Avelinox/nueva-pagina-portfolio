0ec08d5120614a2f511e98e5f353133e99d965be
Avelinox <86523849+Avelinox@users.noreply.github.com>
2021-09-24 00:08:09 -0300

    validacão finalizado com sucesso

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
 const expresiones = {
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
@@ -11,9 +10,7 @@ const expresiones = {
 }
 
 const campos = {
-	usuario: false,
 	nombre: false,
-	password: false,
 	correo: false,
 	telefono: false,
 	mensagem: false
@@ -21,18 +18,8 @@ const campos = {
 
 const validarFormulario = (e) => {
 	switch (e.target.name) {
-		case "usuario":
-			validarCampo(expresiones.usuario, e.target, 'usuario');
-		break;
 		case "nombre":
 			validarCampo(expresiones.nombre, e.target, 'nombre');
-		break;
-		case "password":
-			validarCampo(expresiones.password, e.target, 'password');
-			validarPassword2();
-		break;
-		case "password2":
-			validarPassword2();
 		break;
 		case "correo":
 			validarCampo(expresiones.correo, e.target, 'correo');
@@ -64,26 +51,6 @@ const validarCampo = (expresion, input, campo) => {
 	}
 }
 
-const validarPassword2 = () => {
-	const inputPassword1 = document.getElementById('password');
-	const inputPassword2 = document.getElementById('password2');
-
-	if(inputPassword1.value !== inputPassword2.value){
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
-		campos['password'] = false;
-	} else {
-		document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
-		document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
-		document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
-		document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
-		document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
-		campos['password'] = true;
-	}
-}
 
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
@@ -94,7 +61,7 @@ formulario.addEventListener('submit', (e) => {
 	e.preventDefault();
 
 	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+	if(campos.nombre && campos.correo && campos.mensagem && campos.telefono && terminos.checked ){
 		formulario.reset();
 
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
@@ -109,84 +76,3 @@ formulario.addEventListener('submit', (e) => {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
 	}
 });
-
-
-
-
-/*
-const form = document.getElementById('user-form');
-const submitButton = document.getElementById('submit-btn');
-
-let timeout = null;
-
-let errors = {
-  nickName: true,
-  email: true,
-  password: true,
-};
-
-const mailformatRegex = /^[^@]+@\w+(\.\w+)+\w$/;
-
-document.querySelectorAll('.form-box').forEach((box) => {
-  const boxInput = box.querySelector('input');
-
-  boxInput.addEventListener('keydown', (event) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      console.log(`Input ${boxInput.name} value: `, boxInput.value);
-
-      validation(box, boxInput);
-    }, 300);
-  });
-});
-
-validation = (box, boxInput) => {
-  if (boxInput.value == '') {
-    showError(true, box, boxInput);
-  } else {
-    showError(false, box, boxInput);
-  }
-
-  if (boxInput.name == 'email') {
-    if (!boxInput.value.match(mailformatRegex)) {
-      showError(true, box, boxInput);
-    } else {
-      showError(false, box, boxInput);
-    }
-  }
-
-
-
-  submitController();
-};
-
-showError = (check, box, boxInput) => {
-  if (check) {
-    box.classList.remove('form-success');
-    box.classList.add('form-error');
-    errors[boxInput.name] = true;
-  } else {
-    box.classList.remove('form-error');
-    box.classList.add('form-success');
-    errors[boxInput.name] = false;
-  }
-};
-
-submitController = () => {
-  console.log(errors);
-  if (errors.nickName || errors.email) {
-    submitButton.toggleAttribute('disabled', true);
-  } else {
-    submitButton.toggleAttribute('disabled', false);
-  }
-};
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  console.log([...formData]);
-  for (let [key, value] of formData.entries()) {
-    console.log(`${key}: ${value}`);
-  }
-});
-*/
